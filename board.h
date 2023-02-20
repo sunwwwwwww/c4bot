415b3e20cde84a1e030ee538731cbc6b24f4af61
sunww <aditmethre19@gmail.com>
2023-02-20 13:14:30 +0530

    please god this is the final commit

--- board.h
+++ board.h
@@ -3,7 +3,7 @@
 
 #include "square.h"
 
-void board_init(square player_colour);
+void board_init();
 void board_display();
 void board_accept_input(square player_colour);
 void board_bot_move(square player_colour);
